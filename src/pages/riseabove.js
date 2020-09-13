38fab5f026738896908ecc53eb77e17c602f3891
A7L45 X4V13R <a7l45x4v13r@RISEs-MacBook-Pro.local>
2020-09-12 17:42:51 -0700

    restyled mobile < 600px rise above logo

--- src/pages/riseabove.js
+++ src/pages/riseabove.js
@@ -7,11 +7,13 @@ import './styles.scss';
 import { motion } from 'framer-motion';
 import BackgroundImage from '../../static/Untitled-1.jpg';
 import BackgroundImage2 from '../../static/Untitled-2.jpg';
-import RiseAboveLogo from '../../static/riseabove.svg'
+import RiseAboveLogo from '../../static/riseabove.svg';
 
 
 
 const RiseAbove = () => {
+
+
   return (
     <div className="container">
       <CamcoNavbar />
@@ -21,6 +23,9 @@ const RiseAbove = () => {
           <Bubbles />
           <motion.img className="riseabove-logo" src={RiseAboveLogo} key={RiseAboveLogo}/>
         </motion.div>
+        <h3>We assist and guide our clients into achieving their most ambitious business development goals, and strategic objectives.</h3><br/>
+        <p>With the highest levels of professionalism and experience, we provide strategic counsel, creative solutions, and prompt, responsive services. Rise Above helps you massively break through any barriers to bring your contribution to delighted new customers in many different lands, across many different seas. We will assist you to capitalize on that success in bringing branding and company image; to give repetitive sales and create a number of splashes in the Domestic and International Business market.</p>
+        <h3 className="heading-center">RISE ABOVE CREATIVE SOLUTIONS</h3>
         <p>A full service marketing agency that specializes in helping clients
         maximize their business potential by allowing them to focus on their strengths
         and priorities while outsourcing their marketing.</p>
@@ -28,7 +33,7 @@ const RiseAbove = () => {
         non-traditional, and grassroots initiatives with an entrepreneurial approach
         tailored to meet the goals and objectives of our clients. RACS, takes a partnership
         approach when it comes to building relationships with our clients.</p>
-        <h3 className="heading-center heading-cta">DIGITAL MEDIA AND ADVERTISING</h3>
+        <h4 className="heading-center heading-cta">DIGITAL MEDIA AND ADVERTISING</h4>
         <h5>EMAIL PUBLICATIONS</h5>
         <p>Very similar to the traditional Sunday paper, digital/web blasts serve to
         keep personal members up to date on company information and details.</p>
@@ -38,7 +43,7 @@ const RiseAbove = () => {
         <h5>AD SPONSORS / ENDORSEMENT MANAGMENT</h5>
         <p>To fully maximize on market expansion, we find it beneficial to partner with supporting
         products and brands that compliment your business.</p>
-        <h3 className="heading-center heading-cta">MARKETING</h3>
+        <h4 className="heading-center heading-cta">MARKETING</h4>
         <h5>BRAND CONSULTATION / STRATEGIC PLANNING</h5>
         <p>Thoroughly map out and develop the best fit of approach for your brand.</p>
         <h5>BRAND IDENTITY</h5>
@@ -49,7 +54,7 @@ const RiseAbove = () => {
         <h5>SOCIAL MEDIA</h5>
         <p>In the last decade or so, social media has been proven to be a top method of brand marketing, publications,
         and promotions. A strong social media presence is a vital sector of our marketing services.</p>
-        <h3 className="heading-center heading-cta">ADVERTISING</h3>
+        <h4 className="heading-center heading-cta">ADVERTISING</h4>
         <h5>STRATEGIC PLANNING</h5>
         <p>We value personal company visions and messages. Strategy is where smart will always meet clever.</p>
         <h5>CAMPAIGN DEVELOPMENT</h5>
@@ -61,6 +66,7 @@ const RiseAbove = () => {
         <h5>WEB & APP DESIGN/DEVELOPMENT</h5>
         <p>We specialize in clean cut user friendly web and mobile design and development including vivid and purposeful
         SEO, SEM, PPC, and more.</p>
+        <br/>
         <motion.img className="background-image" key={BackgroundImage2} src={BackgroundImage2} initial={{ opacity: 0}} animate={{opacity: 1}} transition={{ duration: 0.25 }}/>
       </div>
       <motion.img className="background-image-x" key={BackgroundImage} src={BackgroundImage} initial={{ opacity: 1}} animate={{opacity: 1}} transition={{ duration: 1.75 }}/>
