42c4a9b7b078e5684d0e0711b3a68af6771598da
A7L45 X4V13R <a7l45x4v13r@RISEs-MacBook-Pro.local>
2020-09-23 16:00:36 -0700

    added footer for desktop and added logo for RACS

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Particles from './particles.js'
 import '../pages/styles.scss';
+import Facebook from '../../static/facebook.svg';
+import Linkedin from '../../static/linkedin.svg';
+import Instagram from '../../static/instagram.svg';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
     <>
       <div className="footer">
+        <div className="follow-us">
+          <div className="social-icon">
+            <Link to="https://www.facebook.com/CamCoCommercialinc"><motion.img src={Facebook} key={Facebook} /></Link>
+          </div>
+          <div className="social-icon">
+            <Link to="https://www.linkedin.com/company/camco-commercial-inc/"><motion.img src={Linkedin} key={Linkedin} /></Link>
+          </div>
+          <div className="social-icon">
+            <Link to="https://www.instagram.com/camcocommercial_inc/"><motion.img src={Instagram} key={Instagram} /></Link>
+          </div>
+        </div>
         <div className="term-privacy">
         <Particles />
-        <Link className="nav-item" to="/terms/">Terms</Link>
-        <span className="dot">&#183;</span>
-        <Link className="nav-item" to="/privacy/">Privacy</Link>
+        <Link to="/terms/">Terms<span className="dot">    &#183;    </span></Link>
+        <Link to="/privacy/">Privacy</Link>
         </div>
       </div>
     </>
