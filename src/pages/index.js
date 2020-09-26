937ed9883aaac4b201988805eef634845d85b1de
A7L45 X4V13R <a7l45x4v13r@RISEs-MacBook-Pro.local>
2020-09-25 17:41:44 -0700

    fixed infinite scrolling background on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -137,6 +137,12 @@ const Home = ({ isVisible }) => {
             </motion.div>
           </motion.div>
 
+          <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 6}}>
+            <motion.div className="mobile-nav-spacing">
+              <Link className="mobile-nav-item" to="/press/">PRESS</Link>
+            </motion.div>
+          </motion.div>
+
           <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 7}}>
             <motion.div className="mobile-nav-spacing">
               <Link className="mobile-nav-item" to="/contact/">CONTACT</Link>
