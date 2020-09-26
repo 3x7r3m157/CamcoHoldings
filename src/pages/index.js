58752958b40da917b5e6349925f0c3777253c743
A7L45 X4V13R <a7l45x4v13r@RISEs-MacBook-Pro.local>
2020-09-26 13:37:07 -0700

    rarrrr

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,54 @@ const Home = ({ isVisible }) => {
     <>
       <MobileView>
         <div className="intro-container">
-        <MobileNav/>
-        <Particles />
-        <motion.img className="background-image-x" key={BackgroundImage} src={BackgroundImage} initial={{ opacity: 0}} animate={{opacity: 1}} transition={{ duration: 0.75 }}/>
+          <Particles />
+          <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 1}}>
+            <motion.div>
+              <Link className="hamburger" to="/mobile/"><motion.h1 className="cclogo-mobile" initial={{opacity: 0}} animate={{opacity: 1}} transition={{ duration: 2.5}}>CamCo</motion.h1></Link>
+            </motion.div>
+          </motion.div>
+            <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 1}}>
+              <motion.div className="mobile-nav-spacing">
+                <Link className="mobile-nav-item" to="/about/">ABOUT</Link>
+              </motion.div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 2}}>
+              <motion.div className="mobile-nav-spacing">
+                <Link className="mobile-nav-item" to="/companies/">COMPANIES</Link>
+              </motion.div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 3}}>
+              <motion.div className="mobile-nav-spacing">
+                <Link className="mobile-nav-item" to="/philanthropy/">PHILANTHROPY</Link>
+              </motion.div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 4}}>
+              <motion.div className="mobile-nav-spacing">
+                <Link className="mobile-nav-item" to="/riseabove/">RISE ABOVE</Link>
+              </motion.div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 5}}>
+              <motion.div className="mobile-nav-spacing">
+                <Link className="mobile-nav-item" to="/team/">OUR TEAM</Link>
+              </motion.div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 6}}>
+              <motion.div className="mobile-nav-spacing">
+                <Link className="mobile-nav-item" to="/press/">PRESS</Link>
+              </motion.div>
+            </motion.div>
+
+            <motion.div initial={{ opacity: 0 }} animate={{opacity: 1}} transition={{duration: 7}}>
+              <motion.div className="mobile-nav-spacing">
+                <Link className="mobile-nav-item" to="/contact/">CONTACT</Link>
+              </motion.div>
+            </motion.div>
+          <motion.img className="background-image-x" key={BackgroundImage} src={BackgroundImage} initial={{ opacity: 0}} animate={{opacity: 1}} transition={{ duration: 0.75 }}/>
         </div>
       </MobileView>
       <BrowserView>
